6511b3b7b8e7f1b975a42672f0ec2d957384f8b5
daewon <daewon6905@gmail.com>
2026-06-01 17:28:05 +0900

    조합은 start값으로 자기 자신을 거르므로 visited 배열 불필요

--- algorithm_java/src/combination/ex01.java
+++ algorithm_java/src/combination/ex01.java
@@ -7,25 +7,21 @@ public class ex01 {
     int[]nums = new int[]{1,2,3,4,5};
     List<List<Integer>> answer = new ArrayList<>();
 
-    void dfs(int[] nums, int m, int start, List<Integer> curr, boolean[] visited) {
+    void dfs(int[] nums, int m, int start, List<Integer> curr) {
         if (curr.size() == m) {
             answer.add(new ArrayList<>(curr));
             return;
         }
 
         for (int i = start; i < nums.length; i++) {
-            if(!visited[i]){
-                curr.add(nums[i]);
-                visited[i] = true;
-                dfs(nums, m, i + 1, curr, visited);
-                visited[i] = false;
-                curr.remove(curr.size() - 1);
-            }
+            curr.add(nums[i]);
+            dfs(nums, m, i + 1, curr);
+            curr.remove(curr.size() - 1);
         }
     }
 
     List<List<Integer>> solution(int m) {
-        dfs(nums, m, 0, new ArrayList<>(), new boolean[nums.length]);
+        dfs(nums, m, 0, new ArrayList<>());
         return answer;
     }
 
