80d9abfc00817a0eaacfd9c5a5d68a5d13fbfe9d
daewon <daewon6905@gmail.com>
2026-06-05 20:16:55 +0900

    홍팀청팀 bfs 활용 풀이

---
+++ algorithm_java/src/graph/problems/q1/Q2.java
@@ -0,0 +1,42 @@
+package graph.problems.q1;
+
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Queue;
+
+//bfs 활용 풀이
+public class Q2 {
+    public boolean solution(int[][] friends) {
+        int n = friends.length;
+        int[]color = new int[n];
+        Arrays.fill(color,-1);
+
+        for(int i=0;i<n;i++){
+            if(color[i] == -1){
+                if(!bfs(i,friends,color)){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    boolean bfs(int start, int[][]friends, int[]color){
+        Queue<Integer> queue = new ArrayDeque<>();
+        queue.offer(start);
+        color[start] = 0;
+        while(!queue.isEmpty()){
+            int cur = queue.poll();
+            for(int next : friends[cur]){
+                if(color[next] == color[cur]){
+                    return false;
+                }
+                if(color[next] == -1){
+                    color[next] = 1 - color[cur];
+                    queue.offer(next);;
+                }
+            }
+        }
+        return true;
+    }
+}
