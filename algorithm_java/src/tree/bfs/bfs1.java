6da0a5fd5557e8bd6e60321ae67644ec91b9612b
daewon <daewon6905@gmail.com>
2026-06-17 14:12:49 +0900

    트리 인접리스트 구현

---
+++ algorithm_java/src/tree/bfs/bfs1.java
@@ -0,0 +1,27 @@
+package tree.bfs;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+
+//일방향 인접 리스트
+public class bfs1 {
+
+    void solution(List<List<Integer>>tree){
+        bfs(0,tree);
+    }
+
+    //visited가 필요 없는 이유는 트리 특성상 사이클이 없으며, 일방향이기에 방문한곳을 다시 방문하지 않는것이 보장되기 때문.
+    void bfs(int start, List<List<Integer>> tree) {
+        Queue<Integer> queue = new ArrayDeque<>();
+        queue.offer(start);
+
+        while (!queue.isEmpty()) {
+            int cur = queue.poll();
+            for (int nxt : tree.get(cur)) {
+                queue.offer(nxt);
+            }
+        }
+    }
+}
