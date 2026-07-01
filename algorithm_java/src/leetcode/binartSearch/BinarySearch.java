94145e48eef3de9edde46465255f8472f325fcca
daewon <daewon6905@gmail.com>
2026-07-01 18:31:45 +0900

    이진탐색 풀이

---
+++ algorithm_java/src/leetcode/binartSearch/BinarySearch.java
@@ -0,0 +1,27 @@
+package leetcode.binartSearch;
+
+public class BinarySearch {
+    int n;
+    public int search(int[] nums, int target) {
+        n = nums.length;
+        int index = binarySearch(target, nums);
+        if (nums[index] == target) {
+            return index;
+        }
+
+        return -1;
+    }
+    int binarySearch(int target, int[]nums){
+        int low = 0;
+        int high = n-1;
+        while (low < high) {
+            int mid = (low+high)/2;
+            if(target > nums[mid]){
+                low = mid + 1;
+            }else{
+                high = mid;
+            }
+        }
+        return low;
+    }
+}
