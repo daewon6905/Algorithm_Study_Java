4ecabd1551c2fd216fbb70a5dd9e483cb1c346c6
daewon <daewon6905@gmail.com>
2026-07-02 17:48:59 +0900

    twosum 풀이

---
+++ algorithm_java/src/leetcode/twosum/TwoSum.java
@@ -0,0 +1,17 @@
+package leetcode.twosum;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer,Integer> map = new HashMap<>();
+
+        for(int i=0;i<nums.length;i++){
+            int t = target - nums[i];
+            if(map.containsKey(t)) return new int[]{i,map.get(t)};
+            map.put(nums[i],i);
+        }
+        return new int[0];
+    }
+}
