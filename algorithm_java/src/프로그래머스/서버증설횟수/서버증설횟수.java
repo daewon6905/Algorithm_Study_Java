a0528e475c81da040b90eb22a323c3c04108b9e9
unknown <daewon6905@gmail.com>
2026-07-29 12:17:21 +0900

    서버증설횟수 직관적 풀이

---
+++ algorithm_java/src/프로그래머스/서버증설횟수/서버증설횟수.java
@@ -0,0 +1,24 @@
+package 프로그래머스.서버증설횟수;
+
+public class 서버증설횟수 {
+    public int solution(int[] players, int m, int k) {
+        int len = players.length;
+        int[]addedServer = new int[len];
+        int count = 0;
+        for(int i=0;i<len;i++){
+            int currentTimePlayers = players[i];
+            int extraServerNeeded = currentTimePlayers/m;
+            if(extraServerNeeded<=addedServer[i])continue;
+            if(extraServerNeeded>0){
+                int remainder = extraServerNeeded-addedServer[i];
+                count+=remainder;
+                int j=0;
+                while((j<k) && (i+j<len)){
+                    addedServer[i+j]+=remainder;
+                    j++;
+                }
+            }
+        }
+        return count;
+    }
+}
