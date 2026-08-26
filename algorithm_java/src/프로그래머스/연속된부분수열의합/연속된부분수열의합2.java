cb9bfa64a01569509daf4ae4725d32374e14522b
unknown <daewon6905@gmail.com>
2026-08-26 16:23:48 +0900

    연속된 부분수열의 합 풀이2

--- algorithm_java/src/프로그래머스/연속된부분수열의합/연속된부분수열의합2.java
+++ algorithm_java/src/프로그래머스/연속된부분수열의합/연속된부분수열의합2.java
@@ -1,13 +1,17 @@
 package 프로그래머스.연속된부분수열의합;
 
-public class 연속된부분수열의합 {
+public class 연속된부분수열의합2 {
     public int[] solution(int[] sequence, int k) {
         int[] answer = new int[2];
         int min = Integer.MAX_VALUE;
         int left = 0;
-        int right = 0;
-        int sum = sequence[0];
-        while(left <= right){
+        int sum = 0;
+        for(int right=0;right<sequence.length;right++){
+            sum += sequence[right];
+            while(sum > k && left<=right){
+                sum -= sequence[left];
+                left++;
+            }
             if(sum == k){
                 int currentLen = right - left + 1;
                 if(min > currentLen){
@@ -15,15 +19,6 @@ public class 연속된부분수열의합 {
                     answer[1] = right;
                     min = currentLen;
                 }
-            }
-            if(sum < k){
-                right++;
-                if(right < sequence.length) {
-                    sum += sequence[right];
-                }
-            }else{
-                sum-=sequence[left];
-                left++;
             }
         }
         return answer;
